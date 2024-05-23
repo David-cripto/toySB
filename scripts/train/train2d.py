fbadaa119345060ee277b4c350efe3d59b315e1e
pavel.tikhomirov <pavel.tikhomirov@skoltech.ru>
2024-05-23 23:38:50 +0000

    delete exp_int from arguments

--- scripts/train/train2d.py
+++ scripts/train/train2d.py
@@ -36,9 +36,7 @@ def create_arguments():
     parser.add_argument("--val_log", type=int, default=100, help="number of points to log from validation dataset")
     parser.add_argument("--ddpm", action="store_true", help="use DDPM")
     parser.add_argument("--ot_ode", action="store_true", help="use OT-ODE")
-    parser.add_argument("--exp_int", action="store_true", help="use Exponential Integrator")
     parser.add_argument("--nfe", type=int, default=1000, help="number of function evaluations")
-    parser.add_argument("--ab_order", type=int, default=0, help="order of polynom in Exponential Integrator")
 
     opt = parser.parse_args()
 
@@ -49,7 +47,7 @@ def create_arguments():
     return opt
 
 def main(opt):
-    assert sum([opt.ddpm, opt.ot_ode, opt.exp_int]) == 1, "Should be only one regime of sampling during training"
+    assert sum([opt.ddpm, opt.ot_ode]) == 1, "Should be only one regime of sampling during training"
     logger = Logger(opt.log_dir)
     logger.info("toySB training")
     train_pair_dataset, dim = get_pair_dataset(opt.n_samples, opt.dataset1, opt.dataset2, logger, path_to_save=opt.path_to_save, regime = "train")
